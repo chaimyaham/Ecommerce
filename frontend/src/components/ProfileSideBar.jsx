df5128f927d04b19a80774878f3928a986eb795c
Kawtar  Mahy <kawtarmahy@Kawtars-Air.Home>
2023-06-08 23:20:51 +0100

    dashboard setting

--- frontend/src/components/ProfileSideBar.jsx
+++ frontend/src/components/ProfileSideBar.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiFillDashboard, AiFillMessage, AiOutlineLogout, AiOutlineUser } from 'react-icons/ai'
 import { HiMenuAlt3 } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
 
 const ProfileSideBar = ({active,setActive}) => {
     const menus=[
-        { name:"dahboard",mumber:1,icon: AiFillDashboard},
-        { name:"User",mumber:2,icon: AiOutlineUser},
-        { name:"Inbox",mumber:3,icon: AiFillMessage},
-        { name:"Logout",mumber:5,icon: AiOutlineLogout},
+        { name:"dahboard",number:1,icon: AiFillDashboard},
+        { name:"User",number:2,icon: AiOutlineUser},
+        { name:"Inbox",number:3,icon: AiFillMessage},
+        { name:"Logout",number:4,icon: AiOutlineLogout},
     
     ]
+    const [open,setOpen]=useState(true)
   return (
-    <div className='bg-[#16171F]  min-h-screen w-72 text-[#C4AA4F] px-4'>
+    <div className={`bg-[#16171F]  min-h-screen ${open? 'w-72' : 'w-16'} text-[#C4AA4F] px-4`}>
         <div className='py-3 flex justify-end'>
-            <HiMenuAlt3 size={26} className='cursor-pointer  '/>
+            <HiMenuAlt3 size={26} className='cursor-pointer' onClick={()=>setOpen(!open)}/>
 
         </div>
         <div className='mt-4 flex flex-col  gap-4 realtive '>
@@ -22,14 +23,15 @@ const ProfileSideBar = ({active,setActive}) => {
                 menus?.map((item,i)=>(
 
         <span
+        onClick={()=>setActive(item.number)}
         key={i}
         className={`pl-3 ${
-          active === 8 ? "text-[red]" : ""
-        } 800px:block hidden`}
+          active === item.number ? "text-[#f3efef]" : ""
+        }  cursor-pointer flex item-center  text-sm gap-3.5 font-medium  p-2 hover:bg-gray-800 rounded-md `}
       >
-          <div>{React.createElement(item.icon,{size:"20"})}</div>
+          <div>{React.createElement(item?.icon,{size:"20"})}</div>
 
-        <h2>{item.name}</h2>
+        <h2 className='text-gray-400'>{item?.name}</h2>
       </span>
                 ))
             }
--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -19,7 +19,7 @@ const ProfilePage = () => {
         ) : (
           <>
             <UserInfo />
-            <div className={`${styles.section} flex bg-[#f5f5f5]`}>
+            <div className={` flex bg-[#f5f5f5]`}>
               <div className="w-[50px] 800px:w-[335px] sticky 800px:mt-0 mt-[18%]">
                 <ProfileSideBar active={active} setActive={setActive} />
               </div>
